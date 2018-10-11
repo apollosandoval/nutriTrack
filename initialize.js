ad9a9eb2cca927277092099b7490b16621a5486a
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-10-10 20:01:18 -0700

    seperated initialization file

---
+++ initialize.js
@@ -0,0 +1,16 @@
+// Initialize Fixed Action Button
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton({
+        hoverEnabled: false
+    });
+});
+
+// Initialize tabs
+$(document).ready(function () {
+    $('.tabs').tabs();
+});
+
+// Initialize Modal
+$(document).ready(function () {
+    $('.modal').modal();
+});
