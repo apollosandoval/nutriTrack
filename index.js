2356c93c2cf3ab1248ed02b5e4ab8f1228f8ebce
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-10-10 19:13:03 -0700

    added form draft layout

---
+++ index.js
@@ -0,0 +1,13 @@
+// Initialization
+// Initialize Fixed Action Button
+$(document).ready(function () {
+    $('.fixed-action-btn').floatingActionButton({
+        toolbarEnabled: true,
+        hoverEnabled: false
+    });
+});
+
+// Initialize tabs
+$(document).ready(function () {
+    $('.tabs').tabs();
+});
