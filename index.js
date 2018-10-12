f2c3c4957d16e94cf61e0bba68307cef45a29585
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-10-12 09:13:46 -0700

    hard coded nutrition info

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ let publishCard = function (e) {
                             </div>
                             <!-- Reveal Nutrition Label -->
                             <div class="card-reveal">
-                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <span class="card-title grey-text text-darken-4">Nutritional Information<i class="material-icons right">close</i></span>
                                 <p>Here is some more information about this product that is only revealed once clicked on.</p>
                             </div>
                         </div>
